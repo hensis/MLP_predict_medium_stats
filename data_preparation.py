31e827b697d80a9999b33a2a9ce13476cc17d8fb
hensis <70587974+hensis@users.noreply.github.com>
2021-05-25 18:15:17 +0200

    Create data_preparation.py

---
+++ data_preparation.py
@@ -0,0 +1,22 @@
+from numpy import array
+
+all_views = []
+
+X = []
+y = []
+
+for i in range(len(all_views)):
+  end_c = i + 10
+  out_end_c = end_c + 4
+  
+  if out_end_c > len(all_views):
+    break
+    
+  X.append(all_views[i:end_c])
+  y.append(all_views[end_c:out_end_c])
+
+X = array(X)
+y = array(y)
+
+for i in range(len(X)):
+    print(X[i], y[i])
